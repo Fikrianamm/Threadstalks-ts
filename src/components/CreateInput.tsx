d8933abea38b5c710b7153772590128b10e6c088
fikrianamm <fikritaufiqulanam1@gmail.com>
2024-04-15 20:39:33 +0700

    [feat] make test pass

--- src/components/CreateInput.tsx
+++ src/components/CreateInput.tsx
@@ -40,7 +40,7 @@ export default function CreateInput(
   return (
     <form onSubmit={handleCreate} className="flex flex-col w-full gap-4">
       <input type="text" className="w-full input" placeholder="Title" value={title} onChange={onChangeTitle} required />
-      {/* <input type="text" className="w-full input" placeholder="Category (opsional)" value={category} onChange={onChangeCategory} /> */}
+      <input type="text" className="w-full input" placeholder="Category (opsional)" value={category} onChange={onChangeCategory} />
       <ReactQuill modules={modules} theme="bubble" placeholder="Description..." className="w-full p-0 py-2 input" value={body} onChange={onChangeBody} />
       <button type="submit" className="p-2 px-4 ml-auto btn w-max">Posting</button>
     </form>
